70ba719bfa1005f0c812950846659fb20971e360
rdoe <r.doering@osb-ag.de>
2019-01-29 08:03:05 +0100

    Projektdateien hinzufügen.

---
+++ BusinessTrips/BusinessTrips/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BusinessTrips.Models;
+
+namespace BusinessTrips.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
